fa30cc7f420b2e58d7d177f7b5c2c6b97b3a32a3
abhigit2017 <abhi.mail1@yahoo.com>
2022-07-13 10:14:28 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+################ VPC #################
+resource "aws_vpc" "main" {
+  cidr_block       = "${var.main_vpc_cidr}"
+  instance_tenancy = "default"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+}
+
+ ################# Subnets #############
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.availability_zone1}"
+
+
+ 
+}
+resource "aws_subnet" "subnet2" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "${var.availability_zone2}"
+
+
+  
+}
